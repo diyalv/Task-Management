16695edba0b2f2559435b73f46b80c7792081c21
diyalv <diyavrghs@gmail.com>
2024-02-27 10:25:29 +0530

    Initial Commit

---
+++ Task manage practice/Task_manage_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_manage_practiceApp.swift
+//  Task manage practice
+//
+//  Created by user2 on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Task_manage_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
